6b1bad072f00ff9ada92765b924b4d3d26be1668
xutreij@yandex.ru <xutreij@yandex.ru>
2021-10-29 15:06:18 +0700

    Перенёс крестит в попапе в начало

--- popup.js
+++ popup.js
@@ -19,7 +19,7 @@ setTimeout(() => {
             let tr = document.createElement('tr');
             let imgStrTd = '<td><img src="' + getBlock[i].img.join('"/></td><td><img src="') + '"/></td>';
 
-            tr.innerHTML += `<td>${i}</td><td>${getBlock[i].date}</td>${imgStrTd}<td class="del" url="${getBlock[i].url}">X</td><td>${getBlock[i].channel}</td>`;
+            tr.innerHTML += `<td class="del" url="${getBlock[i].url}">X</td><td>${i}</td><td>${getBlock[i].date}</td>${imgStrTd}<td>${getBlock[i].channel}</td>`;
 
             body_table.appendChild(tr)
 
